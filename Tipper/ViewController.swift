ba1f02b3656c6a4d1db2e6671e628fd0c271854e
Zeeshan Mughal <zeeshan.jp@gmail.com>
2016-10-02 21:42:44 -0400

    Init

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -10,14 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBAction func amountChanged(_ sender: UITextField) {
+		self.compute()
+	}
+	@IBAction func tipChanged(_ sender: UISegmentedControl) {
+		self.compute()
+	}
+	@IBOutlet weak var tipSegmentedControl: UISegmentedControl!
+	@IBOutlet weak var amountField: UITextField!
+	@IBOutlet weak var totalField: UILabel!
+	@IBOutlet weak var tipField: UILabel!
+	func compute() {
+		let amount = Double(self.amountField.text!) ?? 0.0
+		let tips = [0.18, 0.20, 0.30]
+		let tip = amount * tips[self.tipSegmentedControl.selectedSegmentIndex]
+		let total = amount + tip
+		self.tipField.text = String(format: "$%.2f", tip)
+		self.totalField.text = String(format: "$%.2f", total)
+	}
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		self.amountField.keyboardType = UIKeyboardType.decimalPad
+		self.amountField.becomeFirstResponder()
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+		
 	}
 
 
